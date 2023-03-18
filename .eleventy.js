67de5384f064f78728be30b5600614c7d41777e0
jonathan <uuupah@gmail.com>
2023-03-18 20:34:18 +1030

    add logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/img");
     
   return {
     // pathPrefix: "/twofiveoh_website/",
