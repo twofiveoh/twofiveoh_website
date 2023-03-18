968114cc1d71e2c4b55d2f6d09ae799914565a3b
jonathan <uuupah@gmail.com>
2023-03-19 02:57:01 +1030

    add minifier and rimraf to build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,31 @@
+const htmlmin = require("html-minifier");
+const rimraf = require("rimraf");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/img");
-    
-    eleventyConfig.addCollection('episodesorted', function(collectionApi) {
-      return collectionApi.getFilteredByTag("episode")
-          .sort((a, b) => b.data.epindex - a.data.epindex).reverse();
-   });
+  // delete contents of public to ensure removed files are removed from the final build
+  rimraf.windows.sync("public/");
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+
+  // sort episodes by index
+  eleventyConfig.addCollection('episodesorted', function (collectionApi) {
+    return collectionApi.getFilteredByTag("episode")
+      .sort((a, b) => b.data.epindex - a.data.epindex).reverse();
+  });
+
+  // minify all html files
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
 
   return {
     // pathPrefix: "/twofiveoh_website/",
